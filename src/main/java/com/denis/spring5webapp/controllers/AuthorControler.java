b5ab577bb1c12bad2c0247ae450005ceccb9189a
denisz <dencse@rocketmail.com>
2018-09-13 15:51:45 +0200

    templates added

---
+++ src/main/java/com/denis/spring5webapp/controllers/AuthorControler.java
@@ -0,0 +1,22 @@
+package com.denis.spring5webapp.controllers;
+
+import com.denis.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorControler {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorControler(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
