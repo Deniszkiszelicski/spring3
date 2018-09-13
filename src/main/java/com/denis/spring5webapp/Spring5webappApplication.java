82c261c89d3bba9f9d5b9e197f0da3352ef9bbf1
Denisz <denisz@Deniszs-MacBook-Air.local>
2018-09-13 12:46:18 +0200

    second

---
+++ src/main/java/com/denis/spring5webapp/Spring5webappApplication.java
@@ -0,0 +1,16 @@
+package com.denis.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EntityScan("model")
+
+
+public class Spring5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappApplication.class, args);
+	}
+}
